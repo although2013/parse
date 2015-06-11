c925d315fe78d20c104c44f4d809f02d7503230e
Ge Hao <althoughghgh@gmail.com>
2015-06-11 22:05:19 +0800

    js

--- public/js/main.js
+++ public/js/main.js
@@ -29,16 +29,16 @@ $(document).ready(function () {
     $($('.my-top-fix table thead th')[1]).addClass( "not-show-white" ).text("王王王")
 
     var delt = $($('.a-class table thead')[num]).width() - $('.my-top-fix').width()
-    var left = $($('.a-class table thead')[num]).position()["left"]
+    var left = $($('.a-class table thead')[num]).position()["left"] - $(window).scrollLeft()
     $('.my-top-fix').css("left", (left+"px")).css("height", ($($('.a-class table thead')[num]).height()+'px'))
 
     $('.my-top-fix').css("position", "fixed").css("z-index", "500").css("top","0").css("background-color", "white").css("margin-bottom", "0px").css("padding-bottom", "0px")
-};
+  };
 
 
   setInterval(function() {
     check_position();
-}, 150);
+  }, 150);
 
 
 
